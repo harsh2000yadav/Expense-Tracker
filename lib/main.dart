b17bde98c4dd792ad461f8926365101a71abe239
harsh2000yadav <harsh2000yadav@gmail.com>
2020-05-27 19:46:00 +0530

    Loading image

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final List<Transaction> _userTransactions =[
-     Transaction(id: 't1', title: 'Mobile Recharge', amount: 450 , date: DateTime.now()),
-    Transaction(id: 't2', title: 'Watch', amount: 4500 , date: DateTime.now()),
+    // Transaction(id: 't1', title: 'Mobile Recharge', amount: 450 , date: DateTime.now()),
+    // Transaction(id: 't2', title: 'Watch', amount: 4500 , date: DateTime.now()),
    ];
 
    void _addNewTransaction(String txTitle, double txAmount){
