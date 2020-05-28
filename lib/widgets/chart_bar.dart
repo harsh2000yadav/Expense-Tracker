88d88e99fd8d967634ff6fe2e84be8b04984c821
harsh2000yadav <harsh2000yadav@gmail.com>
2020-05-28 20:58:51 +0530

    Chart Bar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,7 +13,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('₹${spendingAmt.toStringAsFixed(0)}')),
+        FittedBox(
+          
+        child: Text('₹${spendingAmt.toStringAsFixed(0)}')),
         SizedBox(
           height:5,
         ),
