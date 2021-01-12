f823dac7b5fa6255874a1449e299157be6c3527e
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2021-01-12 19:22:58 +0530

    Update transaction.dart

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -14,20 +14,3 @@ class Transaction{
   @required this.date});
 
 }
-
-import 'package:flutter/foundation.dart';
-
-class Transaction{
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-
-
-  Transaction({
-  @required this.id,
-  @required this.title,
-  @required this.amount,
-  @required this.date});
-
-}
